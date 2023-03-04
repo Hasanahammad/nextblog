5c25572c53fbbd393a8867f4072ac935faba6583
Abdul Jabbar Gazi <niloygazi3@gmail.com>
2023-03-04 22:12:16 +0600

    Cross origin

--- ADMIN/routes/api.php
+++ ADMIN/routes/api.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\Api\NewsUploadController;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\Cors;
 
 /*
 |--------------------------------------------------------------------------
@@ -21,8 +22,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::middleware('cors')->group(function () {
+
 //Route::post('/register',[UserController::class, 'register']);
 //Route::post('/NewsUpload',[NewsUploadController::class, 'onNewsUpload']);
 Route::get('/NewsRetrieve/{parameter?}',[NewsUploadController::class, 'onNewsRetrieve']);
 Route::get('/NewsRetrieveOnCategory/{category}',[NewsUploadController::class, 'NewsRetrieveUsingCategory']);
 
+});
