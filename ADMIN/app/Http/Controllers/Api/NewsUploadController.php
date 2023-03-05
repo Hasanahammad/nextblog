7aaccd8a1dcc4add7b6e2197f213ace36e2d0653
Abdul Jabbar Gazi <niloygazi3@gmail.com>
2023-03-05 22:15:09 +0600

    test

--- ADMIN/app/Http/Controllers/Api/NewsUploadController.php
+++ ADMIN/app/Http/Controllers/Api/NewsUploadController.php
@@ -52,7 +52,7 @@ class NewsUploadController extends Controller
             //     $row->upload_video = asset('videos/' . $row->upload_video);
             //     $row->thumbnail = asset('thumbnails/' . $row->thumbnail);
             // }
-            return response()->json($result);
+            return response()->$result;
 
         }
     }
